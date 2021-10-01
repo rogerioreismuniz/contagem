bfa4d59840d78688cd9de56fdf88ec1460012cad
FE5-Rogerio <rogerioreismuniz@gmail.com>
2021-09-30 21:32:55 -0300

    add JS

---
+++ index.js
@@ -0,0 +1,10 @@
+var dupla = window.prompt("Digite a quantidade de pessoas para o Baile")
+for (let contador = 1; contador <= dupla; contador = contador + 1) {
+    if (contador % 2 == 0 && contador != 0){
+       console.log(contador + "    PAR   está Classificado! - Bem Vindo ao Baile!")
+    } else if  (contador % 2 != 0) {
+       console.log(contador + "    IMPAR está Desclassificado! - Infelizmente não poderá entrar no Baile!")
+    } else { 
+       console.log(contador + '\t numero "ZER0O" 0')
+    }
+}
